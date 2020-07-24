b552b69fe1ef89622896c54f26d080a43644a89d
Nick Stuivenberg <n.stuivenberg@novi.nl>
2020-07-24 21:46:01 +0200

    Begin magie van JPA query methods beschreven

--- src/main/java/nl/novi/stuivenberg/springboot/example/security/repository/RoleRepository.java
+++ src/main/java/nl/novi/stuivenberg/springboot/example/security/repository/RoleRepository.java
@@ -6,6 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * De JpaRepositories die jullie al kennen. Hier wordt gebruik gemaakt van Query Creation. Op basis van de methode naam
+ * weten Spring en JPA welke query op de database uitgevoerd moet worden.
+ * Hier kan meer informatie over gevonden worden:
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
     Optional<Role> findByName(ERole name);
+
 }
--- src/main/java/nl/novi/stuivenberg/springboot/example/security/repository/UserRepository.java
+++ src/main/java/nl/novi/stuivenberg/springboot/example/security/repository/UserRepository.java
@@ -5,10 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * De JpaRepositories die jullie al kennen. Hier wordt gebruik gemaakt van Query Creation. Op basis van de methode naam
+ * weten Spring en JPA welke query op de database uitgevoerd moet worden.
+ * Hier kan meer informatie over gevonden worden:
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
-
     Boolean existsByUsername(String username);
-
     Boolean existsByEmail(String email);
+
 }
