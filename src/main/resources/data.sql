cfbe3b5300f424393143a7a99e6ab414c77798e9
Nick Stuivenberg <n.stuivenberg@novi.nl>
2020-07-27 08:25:32 +0200

    Auto insert roles in DB
    Klein beetje extra commentaar

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,11 @@ volgende aan application.properties toegevoegd:
 
 spring.datasource.initialization-mode=always
 
+Zoals gezegd, dit is een van de manieren. De huidige opzet avn deze applicatie heeft een vast aantal user-rollen. Deze
+kunnen niet door eindgebruikers, moderators of admins toegevoegd worden. Alleen de programmeur kan user-rollen
+toevoegen. Daarom is er ook geen Service & repo voor de user-rollen geprogrammeerd. De enige manier om dan iets in de
+database te krijgen is via SQL statements in dit bestand.
+
  */
 INSERT INTO role(name) VALUES('ROLE_USER');
 INSERT INTO role(name) VALUES('ROLE_MODERATOR');
